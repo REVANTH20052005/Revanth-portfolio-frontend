e946f0fd50a8f8113bfd0210d871a91c10200b40
V Revanth <130386805+REVANTH20052005@users.noreply.github.com>
2025-09-07 22:32:24 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,7 +5,7 @@ const Projects = () => {
     {
       title: "Web3 Deployer",
       description: "Decentralized hosting platform for deploying websites and dApps using blockchain technology. Features IPFS storage, smart contracts, and Web3Auth for secure authentication.",
-      image: "/web3.png",
+      image: "https://freeimage.host/i/Ko1uxrQ",
       technologies: ["React.js", "TailwindCSS", "Drizzle ORM", "IPFS", "Web3Auth"],
       github: "https://github.com/Shyam-Prasath/web-3-deployer",
       live: "https://web-3-deployer.vercel.app/",
